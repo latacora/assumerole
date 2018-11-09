d3cfe78bf27d3d2fc54f1ca1dfeaf05c4d1b6352
Laurens Van Houtven (lvh) <lvh@latacora.com>
2018-11-08 18:25:49 -0600

    Flip clean environ

--- assumerole.py
+++ assumerole.py
@@ -7,7 +7,7 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("--role-arn", required=True, help="The role ARN to assume. Mandatory.")
 parser.add_argument("--role-session-name", default="tempsession", help="A name for this session.")
-parser.add_argument("--keep-environ", default=True, help="Pass parent environment variables to child")
+parser.add_argument("--empty-environ", action="store_true", help="Pass empty environment to child")
 parser.add_argument("argv", nargs="+", help="Argv for the subcommand to run")
 
 def main():
@@ -19,7 +19,7 @@ def main():
     return subprocess.call(
         args.argv,
         env=dict(
-            os.environ if args.keep_environ else {},
+            {} if args.clean_environ else os.environ,
             AWS_ACCESS_KEY_ID=res["Credentials"]["AccessKeyId"],
             AWS_SECRET_ACCESS_KEY=res["Credentials"]["SecretAccessKey"],
             AWS_SESSION_TOKEN=res["Credentials"]["SessionToken"]
