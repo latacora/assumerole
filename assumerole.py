e159a02b382aaf4b0d4f3773bb2b90f61534ee31
Laurens Van Houtven (lvh) <lvh@latacora.com>
2019-03-24 22:39:56 -0500

    Add session duration parameter

--- assumerole.py
+++ assumerole.py
@@ -7,14 +7,16 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("--role-arn", required=True, help="The role ARN to assume. Mandatory.")
 parser.add_argument("--role-session-name", default="tempsession", help="A name for this session.")
-parser.add_argument("--empty-environ", action="store_true", help="Pass empty environment to child")
+parser.add_argument("--duration-seconds", default=3600, help="Credential lifetime in seconds.")
+parser.add_argument("--empty-environ", action="store_true", help="Pass empty environment to child.")
 parser.add_argument("argv", nargs="+", help="Argv for the subcommand to run")
 
 def main():
     args = parser.parse_args()
     res = botocore.session.get_session().create_client("sts").assume_role(
         RoleArn=args.role_arn,
-        RoleSessionName=args.role_session_name
+        RoleSessionName=args.role_session_name,
+        DurationSeconds=args.duration_seconds
     )
 
     env = dict(
